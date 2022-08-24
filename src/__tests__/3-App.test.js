4fba636ca34f0fdc4ecf7a9ffc28ad6497ab9e0b
Adam Tang <adamtang@gmail.com>
2022-08-24 11:48:36 -0700

    removed unused methods in test

--- src/__tests__/3-App.test.js
+++ src/__tests__/3-App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import App from '../App';
-import { screen, render, fireEvent, cleanup } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
 import data from '../data/fares.json';
